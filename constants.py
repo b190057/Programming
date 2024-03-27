e984966631ff7b225687b227dad7094e7324bc23
b190057 <alvaro.alonso.lancho@alumnos.upm.es>
2024-03-27 01:40:11 +0100

    v0.3.6 constants updated.

--- constants.py
+++ constants.py
@@ -3,3 +3,10 @@ import os
 HOME_PATH = os.getcwd()
 DATA_PATH = os.path.join(HOME_PATH, 'data')
 IMAGES_PATH = os.path.join(HOME_PATH, 'images')
+
+MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+               'November', 'December']
+WEEK_DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+COLORS = ['red', 'blue', 'green', 'yellow', 'orange', 'purple', 'cyan', 'magenta', 'brown', 'pink', 'olive',
+          'teal', 'navy']
